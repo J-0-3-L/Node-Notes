85937e54533e16b63560f11dc0352aa4c8b8f94c
Joel Lawi <abecedegluu@gmail.com>
2024-02-17 18:53:01 -0500

    segundo commit

--- src/database.js
+++ src/database.js
@@ -7,9 +7,5 @@ mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-  .then(() => {
-    console.log("Conexión a la base de datos establecida");
-  })
-  .catch(error => {
-    console.error("Error al conectar a la base de datos:", error);
-  });
+  .then(db=>console.log("Conexión a la base de datos establecida"))
+  .catch(err=>console.log("Error al conectar a la base de datos"))
--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+const path = require('path')
 
 // Inicializacion
 const app = express()
@@ -6,6 +8,13 @@ const app = express()
 // Settings
 app.set('port', process.env.PORT || 4000)
 app.set('views', path.join(__dirname, 'views'))
+app.engine('.hbs', exphbs.engine({
+    defaultLayout:'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname:'.hbs'
+}))
+app.set('view engine', '.hbs')
 
 // Middlewares
 app.use(express.urlencoded({extended:false}))
@@ -14,7 +23,7 @@ app.use(express.urlencoded({extended:false}))
 
 
 // Routes
-
+app.use(require('./routes/index.routes'))
 
 // Static files
 app.use(express.static(path.join(__dirname,'public')))
